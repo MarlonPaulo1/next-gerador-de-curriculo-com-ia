186b03480ffffa82afb09ac5d17a82b8c498c116
Marlon Paulo <marlonpaulo1@gmail.com>
2024-12-12 15:04:49 -0300

    Protegendo rotas da API com honojs

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,63 +1,32 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
-import { z } from "zod"
-import { zValidator } from '@hono/zod-validator'
+import { logger } from 'hono/logger'
+import { HTTPException } from 'hono/http-exception'
+import authors from './authors'
+import books from './books'
 
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app
-    .get('/hello',
-        (x) => x,
-        (x) => x,
-        (x) => x,
-        (x) => x,
-        (c) => {
-            return c.json({
-                message: 'Hello Next.js!',
-            })
-        }
-    )
-    .get("/hello/:id",
-        zValidator(
-            'param',
-            z.object({
-                id: z.string()
-            })
-        ),
-        async (c) => {
-            const { id } = c.req.valid("param")
-            return c.json({
-                message: "Hello Dev",
-                id: id,
-            })
-        }
-    )
-    .post("/create/:postId",
-        zValidator(
-            'json',
-            z.object({
-                title: z.string(),
-                summary: z.string()
-            })
-        ),
-        zValidator(
-            'param',
-            z.object({
-                postId: z.string()
-            })
-        ),
-        async (c) => {
-            const { title, summary } = c.req.valid("json")
-            const { postId } = c.req.valid("param")
-            return c.json({
-                title,
-                summary,
-                postId
-            })
-        }
-    )
+app.use("*", logger())
+
+app.onError((err, c) => {
+    if (err instanceof HTTPException) {
+        return err.getResponse()
+    }
+    return c.json({ error: "internal error" })
+})
+
+const routes = app.route('/authors', authors).route('/books', books)
+
+app.get("/", (c) => {
+    return c.json({
+        message: "Hello from Ai resume!"
+    })
+})
+
+export type AppType = typeof routes
 
 export const GET = handle(app)
 export const POST = handle(app)
